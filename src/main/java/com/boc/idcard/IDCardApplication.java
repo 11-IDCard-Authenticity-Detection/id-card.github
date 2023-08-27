9b33d4e5d50a032d1220b1a515b6ab8e45fb9556
yelsonli <767798711@qq.com>
2023-08-27 18:42:42 +0800

    -init-2-

---
+++ src/main/java/com/boc/idcard/IDCardApplication.java
@@ -0,0 +1,13 @@
+package com.boc.idcard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IDCardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IDCardApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boc/idcard/IDCardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boc.idcard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IDCardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
